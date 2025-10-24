27cc9b3374933b82b3196b86045cf0e3f49ff3f4
felixwyz <wu1824@purdue.edu>
2025-10-24 17:13:50 -0400

    test

--- PawnPiece.hh
+++ PawnPiece.hh
@@ -3,15 +3,21 @@
 
 #include "ChessPiece.hh"
 
-/**
- * Student implementation of a Pawn chess piece.
- * The class is defined within the Student namespace.
- */
 namespace Student
 {
-    class PawnPiece : public ChessPiece
-    {
-    };
+  class PawnPiece : public ChessPiece
+  {
+  public:
+    PawnPiece(ChessBoard &board, Color color, int row, int column);
+    bool canMoveToLocation(int toRow, int toColumn) override;
+    const char *toString() override;
+
+  private:
+    ChessBoard *m_board;
+    Color m_color;
+    int m_row;
+    int m_col;
+  };
 }
 
 #endif
